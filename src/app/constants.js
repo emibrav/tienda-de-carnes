9ea076ff9723429486771e61295b5eaf38c9c4d2
Emiliano BRavo <bravoemiliano74@gmail.com>
2023-09-02 15:16:28 -0300

    cambio en title

--- src/app/constants.js
+++ src/app/constants.js
@@ -2,7 +2,7 @@ const phoneNumber = process.env.REACT_APP_PHONE_NUMBER
 const sheet = process.env.REACT_APP_GOOGLE_SHEET_ENDPOINT
 
 export const INFO = {
-  title: "Tienda de carnes",
+  title: "CERDO VA - Lista de precios por mayor",
   phone: phoneNumber,
   social: {
     instagram: "instagram.com/",
