7a173e09ff42ee727b6c335378a2879775acb6d5
Emiliano BRavo <bravoemiliano74@gmail.com>
2023-10-09 16:06:55 -0300

    first commit

--- src/app/constants.js
+++ src/app/constants.js
@@ -2,7 +2,7 @@ const phoneNumber = process.env.REACT_APP_PHONE_NUMBER
 const sheet = process.env.REACT_APP_GOOGLE_SHEET_ENDPOINT
 
 export const INFO = {
-  title: "CERDO VA - Lista de precios por mayor",
+  title: "DRINKS - DELIVERY DE BEBIDAS",
   phone: phoneNumber,
   social: {
     instagram: "instagram.com/",
